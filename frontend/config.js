fb00cc6e62371d529b5d2e9f109ebb9ff816b050
zakootabai-svg <zakootabai@gmail.com>
2025-09-08 14:37:51 +0500

    Update config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,3 @@
 const API_BASE = location.hostname.includes("localhost")
-  ? "http://localhost:4000" // for local testing
-  : "https://hostel-app-vesi.onrender.com"; // your Render backend
+  ? "http://localhost:4000"
+  : "https://hostel-app-vesi.onrender.com"; // Render backend
